75bb0c5e1566e7f61bc4d539b92647e86bf91b2f
Inosensey <dingcong.bae@gmail.com>
2026-01-15 10:08:05 +0800

    Register UserModule and UserTypeModule, add AuthGuard and PermissionGuard to providers

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,15 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './prisma/prisma.module';
+import { UserModule } from './modules/usersModule/users.module';
+import { UserTypeModule } from './modules/userTypeModule/userType.module';
+import { AuthGuard } from './guards/auth.guard';
+import { PermissionsGuard } from './guards/permission.guard';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, UserModule, UserTypeModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, AuthGuard, PermissionsGuard],
+  exports: [AuthGuard, PermissionsGuard],
 })
 export class AppModule {}
