7b55078d01ddd547a019eed0c0a0609ea15d5435
Inosensey <dingcong.bae@gmail.com>
2026-01-21 18:04:00 +0800

    Add update booking endpoint and refine existing controller functions

--- src/modules/bookingsModule/bookings.controller.ts
+++ src/modules/bookingsModule/bookings.controller.ts
@@ -11,7 +11,11 @@ import { BookingService } from './bookings.service';
 import { AuthGuard } from 'src/guards/auth.guard';
 import { PermissionsGuard } from 'src/guards/permission.guard';
 import { ApiResponse } from 'src/utils/responseShaper';
-import { CreateBookingDTO, UpdateBookingDto } from './bookings.dto';
+import {
+  CreateBookingDTO,
+  RescheduleBookingDto,
+  UpdateStatusBookingDto,
+} from './bookings.dto';
 
 @Controller('Bookings')
 export class BookingsController {
@@ -21,37 +25,50 @@ export class BookingsController {
   @Get('/:talentId')
   async getTalentBookings(@Param('talentId') talentId: string) {
     const results = await this.bookingService.getTalentBookings(talentId);
-    ApiResponse.success(results, 'Bookings retrieved successfully');
+    return ApiResponse.success(results, 'Bookings retrieved successfully');
   }
 
   @UseGuards(AuthGuard, PermissionsGuard)
   @Get('/id')
   async getBookingById(@Param('id') id: string) {
     const results = await this.bookingService.getTalentBookingById(id);
-    ApiResponse.success(results, 'Bookings retrieved successfully');
+    return ApiResponse.success(results, 'Bookings retrieved successfully');
   }
 
   @UseGuards(AuthGuard, PermissionsGuard)
   @Post('/request')
   async requestBooking(@Body() data: CreateBookingDTO) {
     const result = await this.bookingService.requestBooking(data);
-    ApiResponse.success(result, 'Booking created successfully');
+    return ApiResponse.success(result, 'Booking created successfully');
+  }
+
+  @UseGuards(AuthGuard, PermissionsGuard)
+  @Put('/update/:id')
+  async updateBooking(
+    @Param('id') id: string,
+    @Body() data: UpdateStatusBookingDto,
+  ) {
+    const result = await this.bookingService.updateBooking(
+      data.action as 'accept' | 'reject',
+      id,
+    );
+    return ApiResponse.success(result, `Booking ${data.action}ed successfully`);
   }
 
   @UseGuards(AuthGuard, PermissionsGuard)
   @Put('/reschedule/:id')
   async rescheduleBooking(
     @Param('id') id: string,
-    @Body() data: UpdateBookingDto,
+    @Body() data: RescheduleBookingDto,
   ) {
     const result = await this.bookingService.rescheduleBooking(id, data);
-    ApiResponse.success(result, 'Booking rescheduled successfully');
+    return ApiResponse.success(result, 'Booking rescheduled successfully');
   }
 
   @UseGuards(AuthGuard, PermissionsGuard)
   @Put('/cancel/:id')
   async cancelBooking(@Param('id') id: string) {
     const result = await this.bookingService.cancelBooking(id);
-    ApiResponse.success(result, 'Booking canceled successfully');
+    return ApiResponse.success(result, 'Booking canceled successfully');
   }
 }
