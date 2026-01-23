daf7e321384c1d02e4fc8ed532bd01dd45116600
Inosensey <dingcong.bae@gmail.com>
2026-01-23 17:54:56 +0800

    Update the return value of getHello.

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,15 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getHello() {
+    return {
+      name: 'Talend Booking Management API',
+      status: 'running',
+      version: '1.0.0',
+      description:
+        'A backend API for managing bookings, schedules, and related business logic.',
+      docs: 'https://github.com/Inosensey/booking-and-availability-api#api-documentation',
+      timestamp: new Date().toISOString(),
+    };
   }
 }
